9f95d7c7ff2bbf5e8b854a38c8d4fef67d82e0ba
Kenneth Transier <ktransier@gmail.com>
2020-03-05 17:18:24 -0800

    Setting up Github Action test runner

--- lib/firetrain.rb
+++ lib/firetrain.rb
@@ -2,17 +2,17 @@ require 'firetrain/railtie'
 require 'firebase_id_token'
 
 module FireTrain
-  FirebaseIdToken::Certificates.request!
+  FirebaseIdToken::Certificates.request! if ENV['RAILS_ENV'] == 'development'
 
   autoload :Authentication, 'firetrain/authentication'
   module Controllers
-	  autoload :Authenticatable, 'firetrain/controllers/authenticatable'
-	end
+    autoload :Authenticatable, 'firetrain/controllers/authenticatable'
+  end
 
   class Engine < ::Rails::Engine
-	  initializer "firetrain.precompile" do |app|
-	    app.config.assets.paths << Rails.root.join('/engines/firetrain/app/assets/javascripts')
-	    app.config.assets.precompile << "firetrain/application.js"
-	  end
+    initializer "firetrain.precompile" do |app|
+      app.config.assets.paths << Rails.root.join('/engines/firetrain/app/assets/javascripts')
+      app.config.assets.precompile << "firetrain/application.js"
+    end
   end
 end
