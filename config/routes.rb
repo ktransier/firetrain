36c6006f587eb254969b68b9834e26f4843e6a51
Kenneth Transier <ktransier@gmail.com>
2020-03-11 08:32:48 -0700

    Fix set_token_user route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get    '/auth'            => 'session_authentication#new'
   post   '/create_session'  => 'session_authentication#create'
   delete '/sign_out'        => 'session_authentication#destroy'
-  post   '/set_token_user'  => 'token_authentication#new'
+  post   '/set_token_user'  => 'token_authentication#create'
 end
