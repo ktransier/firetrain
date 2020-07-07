f18cd557138cfdc25c62e939bb1d034e85b8f59b
Kenneth Transier <ktransier@gmail.com>
2020-07-06 23:40:30 -0700

    update API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get    '/auth'            => 'session_authentication#new'
-  post   '/create_session'  => 'session_authentication#create'
-  delete '/sign_out'        => 'session_authentication#destroy'
-  post   '/set_token_user'  => 'token_authentication#create'
+  get    '/api/auth'            => 'session_authentication#new'
+  post   '/api/create_session'  => 'session_authentication#create'
+  delete '/api/sign_out'        => 'session_authentication#destroy'
+  post   '/api/set_token_user'  => 'token_authentication#create'
 end
